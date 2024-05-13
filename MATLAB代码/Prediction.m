aa778d43c9c66d2dc78187be26b0bba013bfc2dd
Meng_Ren <3029606296@qq.com>
2024-05-13 15:48:03 +0800

    add control file

---
+++ MATLAB代码/Prediction.m
@@ -0,0 +1,11 @@
+% 优化控制函数，获得输入变量
+function u_k= Prediction(x_k,E,H,N,p)
+
+U_k = zeros(N*p,1); % NP x 1
+
+H=(H+H')/2;
+U_k = quadprog(H,E*x_k);
+
+u_k = U_k(1:p,1); % 取第一个结果
+
+end
---
+++ cpp/01.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> heights(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> heights[i];
+    }
+
+    int peaks = 0;
+
+    // 检查左边界
+    if (heights[0] > heights[1]) {
+        peaks++;
+    }
+
+    // 检查中间部分
+    for (int i = 1; i < n - 1; ++i) {
+        if (heights[i] > heights[i - 1] && heights[i] > heights[i + 1]) {
+            peaks++;
+        }
+    }
+
+    // 检查右边界
+    if (heights[n - 1] > heights[n - 2]) {
+        peaks++;
+    }
+
+    cout << peaks << endl;
+
+    return 0;
+}
